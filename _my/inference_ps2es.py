edce87330507c73a6fb306a06d2eabef8f8e2f19
SiRan Li <lisiran@linctex.com>
2025-06-12 18:49:15 +0800

    添加功能 inference过程中保存用于可视化的数据

--- _my/inference_ps2es.py
+++ _my/inference_ps2es.py
@@ -1,9 +1,9 @@
 # first inference point stitch，then obtain edge stitch
 import torch
 from tqdm import tqdm
-
+import time
 import os.path
-
+import numpy as np
 from model import build_model
 from dataset import build_stylexd_dataloader_inference
 
@@ -22,7 +22,7 @@ if __name__ == "__main__":
         "brep_reso_512",
         "brep_reso_1024"
         ]: raise ValueError(f"data_type{data_type} is not valid")
-
+    lst_tmp = []
     from utils.config import cfg
     from utils.parse_args import parse_args
 
@@ -32,15 +32,21 @@ if __name__ == "__main__":
     # 一些超参数从cfg文件中获得，不用ckpt中的
     model.pc_cls_threshold = cfg.MODEL.PC_CLS_THRESHOLD
     inference_loader = build_stylexd_dataloader_inference(cfg)
-
+    model.to("cpu")
     #是否导出为视频（分图片）
-    export_vis_result=False
+    export_vis_result = False
+    export_vis_source = True
 
-    for batch in tqdm(inference_loader):
+    for idx, batch in tqdm(enumerate(inference_loader)):
+        try:
+            time1 = time.time()
             batch = to_device(batch, model.device)
             inf_rst = model(batch)
             if data_type == "Garmage256":
-                data_id = int(os.path.basename(batch['mesh_file_path'][0]).split("_")[1])
+                try:
+                    data_id = int(os.path.basename(batch['mesh_file_path'][0]).split("_")[1])
+                except Exception:
+                    data_id = idx
                 g_basename = os.path.basename(batch['mesh_file_path'][0])
             else: data_id=int(batch['data_id'])
 
@@ -66,10 +72,10 @@ if __name__ == "__main__":
             if data_type == "Garmage256":
                 stitch_mat_full, stitch_pcs, unstitch_pcs, stitch_indices, stitch_indices_full, logits = (
                     get_pointstitch(batch, inf_rst,
-                                    sym_choice="", mat_choice="hun",
+                                    sym_choice="sym_max", mat_choice="col_max",
                                     filter_neighbor_stitch=True, filter_neighbor = 1,
-                                    filter_too_long=True, filter_length=0.12,
-                                    filter_too_small=True, filter_logits=0.15,
+                                    filter_too_long=True, filter_length=0.1,
+                                    filter_too_small=True, filter_logits=0.16,
                                     only_triu=True, filter_uncontinue=False,
                                     show_pc_cls=False, show_stitch=False))
             # elif data_type == "brep_reso_128":
@@ -83,18 +89,47 @@ if __name__ == "__main__":
             #                         show_pc_cls=False, show_stitch=False, export_vis_result = False))
             else:
                 raise NotImplementedError
-            
             if export_vis_result:
                 export_video_results(batch, inf_rst, stitch_pcs, unstitch_pcs, stitch_indices, logits, data_id,
                                      vid_len=240, output_dir=os.path.join("_tmp","video_rotate"))
+            if export_vis_source:
+                batch_np = {}
+                for k in batch:
+                    if isinstance(batch[k], torch.Tensor):
+                        batch_np[k] = batch[k].detach().cpu()
+                    else:
+                        batch_np[k] = batch[k]
+                inf_rst_np = {}
+                for k in inf_rst:
+                    if isinstance(inf_rst[k], torch.Tensor):
+                        inf_rst_np[k] = inf_rst[k].detach().cpu()
+                    else:
+                        inf_rst_np[k] = inf_rst[k]
+                vis_resource = {
+                    "batch":batch_np,
+                    "inf_rst":inf_rst_np,
+                    "stitch_pcs": stitch_pcs.detach().cpu(),
+                    "unstitch_pcs": unstitch_pcs.detach().cpu(),
+                    "stitch_indices": stitch_indices,
+                    "logits": logits,
+                }
+            else:
+                vis_resource = None
 
             # # 优化缝合关系【OPTIONAL】[有问题，而且不是很必要。。。]
             # optimized_stitch_indices = optimize_pointstitch(batch, inf_rst, stitch_mat_full, stitch_indices_full, show_stitch = True)
 
             # 从点点缝合关系获取边边缝合关系 -------------------------------------------------------------------------------------
+
+            # edgestitch_results = pointstitch_2_edgestitch2(batch, inf_rst,
+            #                                                stitch_mat_full, stitch_indices_full,
+            #                                                unstitch_thresh=3, fliter_len=2,
+            #                                                optimize_thresh_neighbor_index_dis=12,
+            #                                                optimize_thresh_side_index_dis=6)
+
             edgestitch_results = pointstitch_2_edgestitch2(batch, inf_rst,
                                                            stitch_mat_full, stitch_indices_full,
-                                                           unstitch_thresh=5, fliter_len=2,
+                                                           unstitch_thresh=3, fliter_len=2,
                                                            optimize_thresh_neighbor_index_dis=12,
                                                            optimize_thresh_side_index_dis=3)
             garment_json = edgestitch_results["garment_json"]
@@ -108,6 +143,15 @@ if __name__ == "__main__":
             data_dir_list = cfg["DATA"]["DATA_TYPES"]["INFERENCE"]
             data_dir = "+".join(data_dir_list)
             save_dir  = os.path.join(save_dir, data_dir)
-            save_result(save_dir, data_id=data_id, garment_json=garment_json, fig=fig_comp, g_basename=g_basename)
+            save_result(save_dir, data_id=data_id, garment_json=garment_json, fig=fig_comp, g_basename=g_basename
+                        , vis_resource=vis_resource, mesh_file_path=batch['mesh_file_path'][0])
             # input("Press ENTER to continue")
             torch.cuda.empty_cache()
+
+            time2 = time.time()
+            lst_tmp.append([time2-time1])
+            print(np.mean(np.array(lst_tmp)[:,0]))
+        except Exception as e:
+            print(e)
+            print("Failed during processing, continue...")
+            continue
--- utils/inference/save_result.py
+++ utils/inference/save_result.py
@@ -1,7 +1,10 @@
 import json, os
+import pickle
+import shutil
+from glob import glob
 
 
-def save_result(save_dir, data_id=0, garment_json=None, fig=None, g_basename=None):
+def save_result(save_dir, data_id=0, garment_json=None, fig=None, g_basename=None, vis_resource=None, mesh_file_path=None):
     os.makedirs(save_dir, exist_ok=True)
     if g_basename is None:
         garment_dir = os.path.join(save_dir, f"garment_" + f"{data_id}".zfill(5) )
@@ -14,3 +17,11 @@ def save_result(save_dir, data_id=0, garment_json=None, fig=None, g_basename=Non
 
     if fig is not None:
         fig.write_html(os.path.join(garment_dir,"vis_comp.html"))
+
+    if vis_resource is not None:
+        with open(os.path.join(garment_dir,"vis_resource.pkl"), 'wb') as f:
+            pickle.dump(vis_resource, f)
+
+    orig_data_list = glob(os.path.join(mesh_file_path, "original_data", "*.pkl"))
+    assert len(orig_data_list) == 1
+    shutil.copy(orig_data_list[0], os.path.join(garment_dir, "orig_data.pkl"))
